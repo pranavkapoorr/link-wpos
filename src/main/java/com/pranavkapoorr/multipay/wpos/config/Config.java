f9ec90d2bc0cc6c528fe43d8ae3f836ac20a2d0c
Pranav Kapoor <pranavkapoorr@gmail.com>
2021-03-07 18:04:11 -0500

    banner/details

--- src/main/java/com/pranavkapoorr/multipay/wpos/config/Config.java
+++ src/main/java/com/pranavkapoorr/multipay/wpos/config/Config.java
@@ -2,7 +2,6 @@ package com.pranavkapoorr.multipay.wpos.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.socket.config.annotation.*;
 import com.pranavkapoorr.multipay.wpos.handler.MessageHandler;
 
